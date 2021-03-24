95b5a65126c62f76aad673427b2f442630e52406
crazychukz <chukwuemekacharles88@gmail.com>
2021-03-24 18:56:24 +0100

    Updated environment variable

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,6 @@ export const environment = {
     url: 'crazychukz', // the auth0 domain prefix
     audience: 'ccm-capstone', // the audience set for the auth0 app
     clientId: 'zAEQVz1mPVonXlKmdAbicMT9C9krAKfD', // the client id generated for the auth0 app
-    callbackURL: 'http://localhost:4200/home', // the base url of the running ionic application.
+    callbackURL: 'https://ccm-capstone-ui.herokuapp.com/home', // the base url of the running ionic application.
   }
 };
